088d8b2bec21edd1153988cb118ee0f6db85b836
Heldeb <helene.deblock@gmail.com>
2026-07-02 12:09:17 +0200

    Update User.php

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -1,5 +1,5 @@
 <?php
-
+// yg
 namespace App\Entity;
 
 use App\Repository\UserRepository;
@@ -103,7 +103,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function __serialize(): array
     {
         $data = (array) $this;
-        $data["\0".self::class."\0password"] = hash('crc32c', $this->password);
+        $data["\0" . self::class . "\0password"] = hash('crc32c', $this->password);
 
         return $data;
     }
